05e7d9bfebc13f1229cc3ae8cd6d7f0e40672ce9
Fabian Kästner <8317993+fabiankaestner@users.noreply.github.com>
2023-02-21 22:04:05 +0100

    feat: add various conversion methods

--- ogn-aprs-parser/src/ogn/utils.rs
+++ ogn-aprs-parser/src/ogn/utils.rs
@@ -1,4 +1,10 @@
-use crate::parser::position::{ParsedDegrees, ParsedDirection::*};
+use chrono::{DateTime, Datelike, TimeZone, Utc};
+use std::f32::consts::PI;
+
+use crate::parser::{
+    position::{ParsedDegrees, ParsedDirection::*},
+    time::ParsedTime,
+};
 
 pub fn parsed_degrees_to_decimal(parsed: ParsedDegrees) -> f32 {
     let deg = parsed.degrees as f32;
@@ -11,6 +17,36 @@ pub fn parsed_degrees_to_decimal(parsed: ParsedDegrees) -> f32 {
         West => -1.0,
     };
     (deg + min + sec) * multiplier
+}
+
+pub fn parsed_time_to_datetime(
+    parsed_time: ParsedTime,
+    date: Option<DateTime<Utc>>,
+) -> DateTime<Utc> {
+    // We only get a time from the network, if we don't get passed a date, assume the message was emitted today.
+    // TODO: this might cause bugs when running around 00:00 UTC, as the rollover to the next day might be slightly offset.
+    let date = date.unwrap_or(Utc::now());
+    Utc.with_ymd_and_hms(
+        date.year(),
+        date.month(),
+        date.day(),
+        parsed_time.elements.0.try_into().unwrap_or(0),
+        parsed_time.elements.1.try_into().unwrap_or(0),
+        parsed_time.elements.2.try_into().unwrap_or(0),
+    )
+    .unwrap()
+}
+
+pub fn feet_to_m(feet: f32) -> f32 {
+    feet * 0.3048
+}
+
+pub fn knots_to_m_s(knots: f32) -> f32 {
+    knots * 463.0 / 900.0
+}
+
+pub fn turn_rate_to_rad_s(turn: f32) -> f32 {
+    turn * PI / 60.0
 }
 
 #[cfg(test)]
@@ -28,6 +64,7 @@ mod tests {
         let nyc_dd = 40.71417;
         assert_eq!(parsed_degrees_to_decimal(nyc_dms), nyc_dd);
     }
+
     #[test]
     fn degrees_to_decimal_returns_negative_for_west() {
         let dms = ParsedDegrees {
@@ -38,5 +75,20 @@ mod tests {
         };
         let dd = -40.71417;
         assert_eq!(parsed_degrees_to_decimal(dms), dd);
+    }
+
+    #[test]
+    fn knots_to_m_s_returns_accurate_result() {
+        assert_eq!(knots_to_m_s(1.0), 0.5144444444444);
+    }
+
+    #[test]
+    fn feet_to_m_returns_accurate_result() {
+        assert_eq!(feet_to_m(1.0), 0.3048);
+    }
+
+    #[test]
+    fn turn_rate_to_rad_s_returns_accurate_result() {
+        assert_eq!(turn_rate_to_rad_s(1.0), 0.05235988);
     }
 }
