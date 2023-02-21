0c6d4165bbaac5bbeaab94101ac1c3a9843c73ed
Fabian Kästner <8317993+fabiankaestner@users.noreply.github.com>
2023-02-21 22:02:59 +0100

    feat: create object position from raw parsed values

--- ogn-aprs-parser/src/model/ogn_object_position.rs
+++ ogn-aprs-parser/src/model/ogn_object_position.rs
@@ -1,7 +1,36 @@
+use crate::ogn::utils::parsed_degrees_to_decimal;
+use crate::parser::{
+    extensions::position_precision::ParsedPositionPrecision,
+    position::ParsedPosition,
+};
+
 #[derive(Debug, PartialEq)]
 pub struct OGNObjectPosition {
     latitude: f32,
     longitude: f32,
     heading: usize,
     altitude: usize,
+}
+
+impl OGNObjectPosition {
+    pub fn from(parsed: ParsedPosition, precision: Option<ParsedPositionPrecision>) -> Self {
+        let p = if let Some(x) = precision {
+            let mut new_parsed = parsed.clone();
+            new_parsed.latitude.seconds_decimal = parsed.latitude.seconds_decimal + x.latitude;
+            new_parsed.longitude.seconds_decimal = parsed.latitude.seconds_decimal + x.longitude;
+            new_parsed
+        } else {
+            parsed
+        };
+
+        let latitude = parsed_degrees_to_decimal(p.latitude);
+        let longitude = parsed_degrees_to_decimal(p.longitude);
+
+        OGNObjectPosition {
+            latitude,
+            longitude,
+            heading: p.heading,
+            altitude: p.altitude,
+        }
+    }
 }
