7c80bcacea408f7b383e8810ad05fb2516440940
Fabian Kästner <8317993+fabiankaestner@users.noreply.github.com>
2023-02-22 12:16:53 +0100

    feat: add test binary to parse text log

---
+++ ogn-aprs-parser/src/bin/parse-log/main.rs
@@ -0,0 +1,31 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+use ogn_aprs_parser::model::ogn_status_message::OGNStatusMessage;
+
+fn read_lines(filename: String) -> io::Lines<BufReader<File>> {
+    println!("{}", filename);
+    let file = File::open(filename).unwrap();
+    io::BufReader::new(file).lines()
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[2];
+
+    let lines = read_lines(filename.to_string());
+    for line in lines {
+        let unwrapped = line.unwrap();
+        let str = unwrapped.as_str();
+        let parsed = OGNStatusMessage::from_str(str, None);
+        if let Ok(p) = parsed {
+            println!(
+                "{}: {},{}",
+                p.aircraft_id.unwrap_or("NO ID".to_owned()), p.position.latitude, p.position.longitude
+            );
+        } else {
+            println!("Parse error.");
+        }
+    }
+}
